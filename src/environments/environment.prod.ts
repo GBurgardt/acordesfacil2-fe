7a9505a40f25ca33f9cb770e5e560012b3495616
GBurgardt <germanandresburgardt@gmail.com>
2018-12-17 14:21:21 -0300

    url heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
     production: true,
-    WS_URL: `http://192.168.0.34:4001/api/v1`,
+    // WS_URL: `http://192.168.0.34:4001/api/v1`,
+    WS_URL: `https://calm-scrubland-84503.herokuapp.com/api/v1`,
     // WS_URL: `http://10.111.64.173:4001/api/v1`,
     TIME_DEBOUNCE: 150
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
     production: false,
-    WS_URL: `http://localhost:4001/api/v1`,
+    // WS_URL: `http://localhost:4001/api/v1`,
+    WS_URL: `https://calm-scrubland-84503.herokuapp.com/api/v1`,
     // WS_URL: `http://192.168.0.11:4001/api/v1`,
     TIME_DEBOUNCE: 150
 };
