a2c2c82900bebf235360ec0f736164f2476524db
Burgardt <germanandresburgardt@gmail.com>
2019-01-07 21:51:35 -0300

    Finish

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,7 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
     production: false,
-    WS_URL: `http://localhost:4001/api/v1`,
+    // WS_URL: `http://localhost:4001/api/v1`,
+    WS_URL: `https://calm-scrubland-84503.herokuapp.com/api/v1`,
     // WS_URL: `http://192.168.0.11:4001/api/v1`,
     TIME_DEBOUNCE: 150
 };
